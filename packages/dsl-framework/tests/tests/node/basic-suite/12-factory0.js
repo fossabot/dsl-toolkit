3b4c94310d42c85330c6c235d8b4415c83fcc837
Imre Toth <tothimre@gmail.com>
2020-04-27 01:09:07 +0200

    Added better tests for the subcommand.

--- packages/dsl-framework/tests/tests/node/basic-suite/12-factory0.js
+++ packages/dsl-framework/tests/tests/node/basic-suite/12-factory0.js
@@ -1,31 +1,25 @@
 module.exports = (curryCallbackObject, expect, enviromentSupportsPromises, dslFrameworkDefaultInstance, dslFramework) => {
   describe('factory functionality', function () {
-    describe('basic usage', function () {
-      it('.Testing subcommand', function () {
-        const data = dslFramework()().x.y.z
-       .a.ddddd('a', '1', 'c').h.j.k
-       .a.ddddd('a', '2', 'c').g.h.j
-       .a.ddddd('a', '3', 'c').ds.s.fa('11')
-       .b.ddddd('a', 'a', 'a')
-       .a('b','c')
-       ()
-       .data
+    it('.Testing subcommand', function () {
+      const data = dslFramework()().x.y.z
+     .a.ddddd('a', '1', 'c').h.j.k
+     .a.ddddd('a', '2', 'c').g.h.j
+     .a.ddddd('a', '3', 'c').ds.s.fa('11')
+     .b.ddddd('a', 'a', 'a')
+     .a('b','c')
+     ()
+     .data
 
-        const r = data.getSubcommand('a')().map((item, i) => {
-          const ddddd = item.command.get('ddddd')
-          const j = item.command.get('j')
-          return {ddddd, j}
-        })
+     const r = data.getSubcommand('a')().map((item, i) => {
+       const ddddd = item.command.get('ddddd')
+       const j = item.command.get('j')
+       return {ddddd, j}
+     })
 
-        console.log(r);
-        r.forEach((e,i) => {
-          if(e.ddddd[0] && e.ddddd[0][0]){
-            if(e.ddddd[0][0] === 'ddddd'){
-              throw 'that should have been dddd'
-            }
-          }
-        });
+     r.forEach((e,i) => {
+       if(typeof e.ddddd === 'undefined')
+        throw 'Well tha should have beeen defined.'
       })
     })
   })
-}
+  }
