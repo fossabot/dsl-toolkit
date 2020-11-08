67c4552ee06089b3f674e90b36dbd8e2bb31f3f5
Imre Toth <tothimre@gmail.com>
2020-11-08 17:48:51 +0100

    .

--- packages/require-a-lot/src/linker.js
+++ packages/require-a-lot/src/linker.js
@@ -2,13 +2,27 @@ const { linkerDir, linkerFile } = require('generic-text-linker')
 const trimRight = require('trim-right')
 const fs = require('fs')
 
-module.exports = (linkFile, begin, end, msg, emptySpaces) => {
+const linking = (linkFile, begin, end, msg, emptySpaces) => {
   msg = msg.split('\n').map(line => trimRight(line)).join('\n')
   const params = [linkFile, begin, end, msg.split('\n')
-    .map(line => emptySpaces + line).join('\n')]
+  .map(line => emptySpaces + line).join('\n')]
   return typeof linkFile === 'string'
     ? fs.lstatSync(linkFile).isDirectory()
       ? linkerDir(...params)
       : linkerFile(...params)
     : {}
+}
+const prepareMsgAsParameters = (msg) => {
+  return msg
+}
+
+module.exports = (linkFile, begin, end, msg, emptySpaces) => {
+  const returningObject = {}
+
+  Object.assign(returningObject,
+    linking(linkFile, begin, end, msg, emptySpaces),
+    linking(linkFile, begin + ' parameters', end + ' parameters', prepareMsgAsParameters(msg), emptySpaces),
+  )
+
+  return returningObject
 }
